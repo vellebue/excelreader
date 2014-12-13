5950849dc0b348a632ac0f4243c0e9e06aef014e
angel <vellebue@yahoo.es>
2014-12-13 13:31:08 +0100

    Fixed warnings.

--- excelreader-core/src/main/java/org/excelreader/core/reader/HssfExcelReader.java
+++ excelreader-core/src/main/java/org/excelreader/core/reader/HssfExcelReader.java
@@ -4,21 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.excelreader.core.model.ExcelDocument;
 
 public class HssfExcelReader extends AbstractExcelReader { 
 
 	public HssfExcelReader(InputStream excelStream) throws IOException {
 		super(new HSSFWorkbook(excelStream));
-		//Workbook workbook = new HSSFWorkbook(excelStream);
 		excelStream.close();
-		//setDocument(buildDocument(workbook));
 	}
-	
-	
-
 }
