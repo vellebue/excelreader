dad290b212f196d4eceb690b9b1ce3bf17934ff8
angel <vellebue@yahoo.es>
2014-12-13 13:05:09 +0100

    Refactored test to add compatibility with new test class XssfExcelReaderTest.

--- excelreader-core/src/test/java/org/excelreader/core/reader/HssfExcelReaderTest.java
+++ excelreader-core/src/test/java/org/excelreader/core/reader/HssfExcelReaderTest.java
@@ -1,28 +1,21 @@
 package org.excelreader.core.reader;
 
-import static org.junit.Assert.*;
-
 import java.io.InputStream;
 
-import org.excelreader.core.model.ExcelDocument;
 import org.junit.Test;
 
-public class HssfExcelReaderTest {
-	
-	@Test
-	public void shouldExtractExcelDataFromASingleSheetExcel() throws Exception{
-		InputStream stream = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream("org/excelreader/core/reader/simpleExcel.xls");
-		HssfExcelReader reader = new HssfExcelReader(stream);
-		ExcelDocument document = reader.getDocument();
-		assertEquals(1, document.getNumberOfSheets());
-		String [][] sheet = document.getSheetAt(0);
-		assertEquals("John", sheet[0][0]);
-		assertEquals("Mary", sheet[0][1]);
-		assertEquals("Paul", sheet[0][2]);
-		assertEquals("Leila", sheet[1][0]);
-		assertEquals("Francis", sheet[1][1]);
-		assertEquals("Jessica", sheet[1][2]);
+public class HssfExcelReaderTest extends AbstractExcelReaderTest {
+
+	@Override
+	public ExcelReader getReaderFromResource(String resourceName) throws Exception {
+		InputStream stream = getInputStreamFromResource(resourceName);
+		return new HssfExcelReader(stream);
 	}
 
+	@Test
+	public void shouldExtractExcelDataFromASingleSheetExcel()
+			throws Exception {
+		ExcelReader reader = getReaderFromResource("org/excelreader/core/reader/simpleExcel.xls");
+		super.shouldExtractExcelDataFromASingleSheetExcel(reader);
+	}
 }
